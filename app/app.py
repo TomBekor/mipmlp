39ace66aab074a604c4fe47e18ed0da9565a390d
arielrz02 <ariel.r.zvi@gmail.com>
2021-07-14 16:04:27 +0300

    reseting host to 0

--- app/app.py
+++ app/app.py
@@ -31,6 +31,6 @@ app.register_blueprint(site_service.bp)
 if __name__ == '__main__':
     with open("static/im_name.txt", "w") as f:
         f.truncate(0)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
